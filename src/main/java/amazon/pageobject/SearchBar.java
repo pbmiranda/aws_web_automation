4e8834f0ef99502e936df1bdad47d39753ec709b
phelipe.miranda <phelipemiranda@gmail.com>
2022-05-22 15:44:26 -0300

    documentation

--- src/main/java/amazon/pageobject/SearchBar.java
+++ src/main/java/amazon/pageobject/SearchBar.java
@@ -1,7 +1,12 @@
 package amazon.pageobject;
 
 import org.openqa.selenium.WebDriver;
-
+/**
+ * 
+ * @author Phelipe Miranda
+ * 
+ * This class represents the SearchBar of AWS Page and its operations
+ */
 public class SearchBar extends AbstractPageComponent{
 
 	public static String SEARCH_BAR_ID = "twotabsearchtextbox";	
@@ -11,10 +16,17 @@ public class SearchBar extends AbstractPageComponent{
 		super(driver);		
 	}
 	
+	/**
+	 * Press the search button 
+	 */
 	public void pressSearchButton() {
 		clickByAttr("id", SEARCH_BUTTON_ID);
 	}
 
+	/**
+	 * Type in the search bar
+	 * @param text - the text that will be typed
+	 */
 	public void type(String text) {
 		typeByAttr("id", SEARCH_BAR_ID, text);
 	}	
